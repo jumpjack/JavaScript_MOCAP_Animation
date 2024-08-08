fddc4660152ed7ef786887c5e76ff9bf7dc0402c
jumpjack <jumpjack@libero.it>
2024-08-08 16:40:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //TYPED.JS animation
 $(function() {
 	$(".type-text").typed({
-		strings:["<br> ^300Hi! <br><br> ^400We are Nina Sun, Brandon Choi, and Cosi Goldstein.<br> ^300Welcome to our final project for CS290: Digital 3D Geometry.<br><br> ^300Press 'ENTER' to continue<br>"],
-		typeSpeed: 50,
+		strings:["<br> ^300Press 'ENTER' to continue<br>"],
+		typeSpeed: 150,
 	});
 });
 
